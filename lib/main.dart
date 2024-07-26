6e64cce07def9fb12fd257a0446438ecdc3af789
titanke <elnatulol@gmail.com>
2024-07-25 23:21:47 -0500

    fix redireccion menu

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home:  Homepage(),
+      home:  Menu(),
           routes: {
       '/detail': (context) => DetailScreen(),
       '/storage': (context) => Storagepage(),
